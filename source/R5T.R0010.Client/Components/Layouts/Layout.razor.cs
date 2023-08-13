e7f24e6af282bbd2202c7267f528543b9f8af5e4
davidcoats <david@getsafetycone.com>
2023-08-12 23:23:30 -0700

    Interim changes.

---
+++ source/R5T.R0010.Client/Components/Layouts/Layout.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0154;
+
+
+namespace R5T.R0010.Client.Layouts
+{
+    /// <summary>
+    /// Default layout.
+    /// </summary>
+    [RazorLayoutMarker]
+    public partial class Layout
+    {
+
+    }
+}
---
+++ source/R5T.R0010/Components/MetaViewportDefault.razor.cs
@@ -0,0 +1,16 @@
+using System;
+
+using R5T.T0154;
+
+
+namespace R5T.R0010.Components
+{
+    /// <summary>
+    /// Supplies the meta viewport tag, with default values width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no.
+    /// </summary>
+    [RazorComponentMarker]
+    public partial class MetaViewportDefault : IRazorComponentMarker
+    {
+
+    }
+}
